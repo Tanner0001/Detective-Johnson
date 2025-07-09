bc5e3ab50b7ce7c7a4b5c2214b9adc0bb92504d8
Shea <shea.cogs@gmail.com>
2025-07-09 13:38:25 -0600

    item collection added

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -6,11 +6,17 @@ public class ItemPickup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Trigger entered by: " + other.name);
+
         var controller = other.GetComponent<InventoryController>();
         if (controller != null)
         {
+            Debug.Log("Item should be picked up");
             controller.CollectItem(item);
             Destroy(gameObject);
         }
     }
+
 }
+
+
